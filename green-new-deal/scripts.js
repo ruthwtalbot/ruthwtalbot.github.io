ea8638aabbe15159024691da772e6a5c58b167fb
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-11-20 16:21:26 -0500

    More gnd updates

--- green-new-deal/scripts.js
+++ green-new-deal/scripts.js
@@ -3,7 +3,9 @@ var container = "#container";
 var colorScale = d3.scaleLinear().domain([0, 1]).range(["#ff0000", "#0000ff"]);
 
 var categories = [
-  "overall",
+  "Overall reported support",
+  "Avg support across subcategories",
+  "Presidential margin 2016",
   "Clean Air and Water",
   "A carbon price",
   "Job guarantee",
@@ -17,8 +19,6 @@ var categories = [
   "Support Minorities",
   "Clean energy agreements",
   "Public transit",
-  "Avg support across",
-  "Presidential Margin 2016",
 ];
 /*
  * Initial function for loading map.
@@ -37,27 +37,37 @@ var categories = [
   d3.csv("Overall_GND_Support.csv").then(function (data) {
     mapData = data;
     let scraped = Object.entries(data[49]).slice(1);
-    scraped = scraped.sort((a, b) => {
-      return b[1] * 1 - a[1] * 1;
+    scraped = scraped
+      .sort((a, b) => {
+        return b[1] * 1 - a[1] * 1;
+      })
+      .filter(s => s[0] != "state_abbrv");
+
+    scraped = scraped.map(function (s) {
+      return { name: s[0], val: [s[1] * 1] };
     });
 
-    scraped = scraped.map(function(s) {
-      return {name: s[0], val: [s[1] * 1]};
-    })
-
     var element = document.querySelector(container);
-    var width = element.offsetWidth * 0.33;
-    var xScale = d3.scaleLinear().domain([0, 1]).range([0, width]);
+    var width = element.offsetWidth * 0.3;
+    var xScale = d3
+      .scaleLinear()
+      .domain([0, 1])
+      .range([0, width - 105]);
 
     var xAxis = d3
       .axisBottom()
       .scale(xScale)
       .ticks(4)
       .tickFormat(function (d) {
-        return d;
+        if (d != 0) return d * 100;
       });
 
-    var chart = d3.select("#charts").append("svg").attr("width", width).attr("height", 500).append("g");
+    var chart = d3
+      .select("#charts")
+      .append("svg")
+      .attr("width", width + 10)
+      .attr("height", width * 3)
+      .append("g");
 
     // Render bars to chart.
     var group = chart
@@ -65,11 +75,11 @@ var categories = [
       .data(scraped)
       .enter()
       .append("g")
-      .attr("transform", (d, i) => "translate(0," + i * (20 + 2) + ")");
+      .attr("transform", (d, i) => "translate(0," + i * (25 + 3) + ")");
     chart
       .append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0, 400)")
+      .attr("transform", `translate(105, ${(scraped.length) * (25 + 3)})`)
       .call(xAxis);
 
     // Add bar chart of average support per measure.
@@ -78,18 +88,63 @@ var categories = [
       .data(d => d.val)
       .enter()
       .append("rect")
-      .attr("x", d => 0)
-      .attr("width", function(d) { return xScale(d * 1)})
-      .attr("height", 20)
+      .attr("x", d => 105)
+      .attr("width", function (d) {
+        return xScale(d * 1);
+      })
+      .attr("height", 25)
       .attr("fill", d => colorScale(d * 1));
 
-    update(mapData, "overall");
+    // Render bar values.
+    group
+      .append("g")
+      .attr("class", "value")
+      .selectAll("text")
+      .data(d => d.val)
+      .enter()
+      .append("text")
+      .text(function (d) {
+        return d ? (d * 100).toFixed(0) + "%" : null;
+      })
+      .attr("class", "bar-text")
+      .attr("x", d => xScale(d * 1) + 90)
+      .attr("text-anchor", "end")
+      .attr("dy", 14);
+
+    // Render bar labels.
+    d3.select("#charts")
+      .append("ul")
+      .attr("class", "labels")
+      .attr(
+        "style",
+        formatStyle({
+          width: "80px",
+          top: "45px",
+          left: "0",
+        })
+      )
+      .selectAll("li")
+      .data(scraped)
+      .enter()
+      .append("li")
+      .attr("style", (d, i) =>
+        formatStyle({
+          width: "100px",
+          height: "28px",
+          left: "0px",
+          top: i * 28 + "px;",
+        })
+      )
+      .attr("class", d => classify(d.name))
+      .append("span")
+      .text(d => d.name);
+
+    update(mapData, "Overall reported support");
   });
 })();
 
 /*
- * Updates data to new slider year selection.
- * @param {number} sliderValue Current slider value.
+ * Updates data to new measure.
  */
 function onSelect() {
   // Display correct data for the radio button checked.
@@ -106,7 +161,7 @@ function update(data, property) {
   var chartElement = containerElement.select("svg");
 
   var element = document.querySelector(container);
-  var width = element.offsetWidth * 0.66;
+  var width = element.offsetWidth * 0.6;
 
   // resize map (needs to be explicitly set for IE11)
   chartElement.attr("width", width).attr("height", function () {
@@ -203,4 +258,14 @@ function updateText(data, property, element) {
 
 function classify(name) {
   return name.split(" ").join("-");
+}
+
+function formatStyle(props) {
+  var s = "";
+
+  for (var key in props) {
+    s += `${key}: ${props[key].toString()}; `;
+  }
+
+  return s;
 }
