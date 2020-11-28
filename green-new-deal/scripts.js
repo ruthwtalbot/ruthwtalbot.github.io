bd97858ece498f95e72ce44727ba5586e188a473
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-11-28 17:32:37 -0500

    Update dem wording

--- green-new-deal/scripts.js
+++ green-new-deal/scripts.js
@@ -5,7 +5,7 @@ var colorScale = d3.scaleLinear().domain([0, 1]).range(["#ff0000", "#0000ff"]);
 var categories = [
   "Overall reported support",
   "Avg support across subcategories",
-  "Democratic vote %",
+  "Democratic vote 2016",
   "Clean Air and Water",
   "A carbon price",
   "Job guarantee",
